a58eefe741c92ec24a1e985b2198d8eeb48556fa
TaimoorRao <taimoor.rao12@gmail.com>
2021-03-25 16:22:14 +0500

    Initial Commit - Mini Quiz App - Build using SQLiteDB for storage of questions and options, SharedPreferences for saving high score and CountDownTimer class for timer.

---
+++ app/src/main/java/com/example/task17/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.task17;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_CODE_QUIZ = 1;
+    public static final String SHARED_PREFS = "sharedPrefs";
+    public static final String KEY_HIGHSCORE = "keyHighScore";
+
+    private TextView textViewHighScore;
+    private int highScore;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textViewHighScore = findViewById(R.id.text_view_highscore);
+        loadHighScore();
+
+        Button buttonStartQuiz = findViewById(R.id.button_start_quiz);
+        buttonStartQuiz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startQuiz();
+            }
+        });
+    }
+
+    private void startQuiz() {
+        Intent intent = new Intent(MainActivity.this, QuizActivity.class);
+        startActivityForResult(intent, REQUEST_CODE_QUIZ);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_QUIZ) {
+            if (resultCode == RESULT_OK) {
+                int score = data.getIntExtra(QuizActivity.EXTRA_SCORE, 0);
+                if (score > highScore) {
+                    updateHighScore(score);
+                }
+            }
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void loadHighScore() {
+        SharedPreferences prefs = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        highScore = prefs.getInt(KEY_HIGHSCORE, 0);
+        textViewHighScore.setText("High Score: " + highScore);
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void updateHighScore(int highScoreNew) {
+        highScore = highScoreNew;
+        textViewHighScore.setText("High Score: " + highScore);
+        SharedPreferences prefs = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt(KEY_HIGHSCORE, highScore);
+        editor.apply();
+    }
+}
---
+++ app/src/main/java/com/example/task17/QuizDbHelper.java
@@ -0,0 +1,90 @@
+package com.example.task17;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.example.task17.QuizContract.*;
+
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class QuizDbHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "MyQuiz.db";
+    private static final int DATABASE_VERSION = 1;
+
+    private SQLiteDatabase db;
+
+    public QuizDbHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        this.db = db;
+
+        final String SQL_CREATE_QUESTIONS_TABLE = "CREATE TABLE " +
+                QuestionTable.TABLE_NAME + " ( " + QuestionTable._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                QuestionTable.COLUMN_QUESTION + " TEXT, " +
+                QuestionTable.COLUMN_OPTION1 + " TEXT, " +
+                QuestionTable.COLUMN_OPTION2 + " TEXT, " +
+                QuestionTable.COLUMN_OPTION3 + " TEXT, " +
+                QuestionTable.COLUMN_ANSWER_NR + " INTEGER" +
+                ")";
+
+        db.execSQL(SQL_CREATE_QUESTIONS_TABLE);
+        fillQuestionTable();
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + QuestionTable.TABLE_NAME);
+        onCreate(db);
+    }
+
+    private void fillQuestionTable() {
+        Question q1 = new Question("A is Correct", "A", "B", "C", 1);
+        Question q2 = new Question("B is Correct", "A", "B", "C", 2);
+        Question q3 = new Question("C is Correct", "A", "B", "C", 3);
+        Question q4 = new Question("A is Correct", "A", "B", "C", 1);
+        Question q5 = new Question("B is Correct", "A", "B", "C", 2);
+        addQuestion(q1);
+        addQuestion(q2);
+        addQuestion(q3);
+        addQuestion(q4);
+        addQuestion(q5);
+    }
+
+    private void addQuestion(Question q) {
+        ContentValues cv = new ContentValues();
+        cv.put(QuestionTable.COLUMN_QUESTION, q.getQuestion());
+        cv.put(QuestionTable.COLUMN_OPTION1, q.getOption1());
+        cv.put(QuestionTable.COLUMN_OPTION2, q.getOption2());
+        cv.put(QuestionTable.COLUMN_OPTION3, q.getOption3());
+        cv.put(QuestionTable.COLUMN_ANSWER_NR, q.getAnswerNr());
+        db.insert(QuestionTable.TABLE_NAME, null, cv);
+    }
+
+    public List<Question> getAllQuestions() {
+        List<Question> questionList = new ArrayList<>();
+        db = getReadableDatabase();
+        Cursor c = db.rawQuery("SELECT * FROM " + QuestionTable.TABLE_NAME, null);
+        if (c.moveToFirst()) {
+            do {
+                Question question = new Question();
+                question.setQuestion(c.getString(c.getColumnIndex(QuestionTable.COLUMN_QUESTION)));
+                question.setOption1(c.getString(c.getColumnIndex(QuestionTable.COLUMN_OPTION1)));
+                question.setOption2(c.getString(c.getColumnIndex(QuestionTable.COLUMN_OPTION2)));
+                question.setOption3(c.getString(c.getColumnIndex(QuestionTable.COLUMN_OPTION3)));
+                question.setAnswerNr(c.getInt(c.getColumnIndex(QuestionTable.COLUMN_ANSWER_NR)));
+                questionList.add(question);
+            } while (c.moveToNext());
+        }
+        c.close();
+        return questionList;
+    }
+}
